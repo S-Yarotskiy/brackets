e37395f1d9af53455e256c4a7171cd0f4c9301f7
S-Yarotskiy <s.v.yarotskiy@gmail.com>
2022-08-07 02:55:09 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig.forEach((e,i,arr) => arr[i] = arr[i].toString().replace(',',''));
+  while (bracketsConfig.some(e => str.includes(e))) {
+    str = str.replace(bracketsConfig[bracketsConfig.findIndex(el => str.includes(el))], '');
+  }
+  return str.length === 0;
 }
